2fdce0b0107c04c9d6b3c588dba6f24273bacce9
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-11-22 20:26:36 -0500

    Wrote pseudo-code for complete program.

---
+++ iou-calculator.py
@@ -0,0 +1,26 @@
+# Program Name: IOU Calculator
+# Program Description: This program calculates debt of each person in a group based on the contents of receipts,
+#                      assuming the prices are split evenly.
+# Date: 11/22/2020
+
+# Create Person class
+
+# Create Receipt class (could just be a dict, but may be nice to have accessor methods)
+
+
+def main():
+    # Initialize list of people in group
+
+    # Prompt for number of people and names
+
+    # Initialize amounts_paid with names of people
+
+    # Prompt for receipts
+
+    # Calculate and print total amounts paid (by each person, and by group as a whole)
+
+    # Calculate and print how much each person should have spent
+
+    # Calculate debt of each person
+
+    # For each person in debt, determine and print how much they owe the people out of debt
